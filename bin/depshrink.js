4961fd669e65d7d98601a685b9ea9c578de9bb1f
alvaro <Alvarojosepl@gmail.com>
2017-02-07 09:35:36 -0600

    added support for multiple commands

--- bin/depshrink.js
+++ bin/depshrink.js
@@ -1,16 +1,21 @@
 #! /usr/bin/env node
-var program = require('commander')
+
+var program = require('commander'),
+    depsShrink = require('../lib/depshrink')
+
+var optionsMap = {
+    list: { delete: false },
+    remove: { delete: false }
+}
 
 program
-    .arguments("list <executable> [dirs...]")
-    .action(function(executable, dirs) {
+    .arguments("<mode> <executable> [scaffolds...]")
+    //.command("list [executable] [dirs...]")
+    .action(function(mode, executable, dirs) {
+        console.log('execute: in mode('+ mode + ') the executable '+ executable + ' in ' + dirs)
+        if(!optionsMap[mode])
+            error(mode + ': command not supported')
         
-    })
-    .parse(process.argv);
-
-program
-    .arguments("remove <executable> [dirs...]")
-    .action(function(executable, dirs) {
-        
+        depsShrink(executable,dirs,optionsMap[mode].delete,true, 1000)
     })
     .parse(process.argv);
