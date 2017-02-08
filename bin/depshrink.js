6ca8678e73845f3af08c56ac70bbeb009182128d
alvaro <Alvarojosepl@gmail.com>
2017-02-08 09:36:14 -0600

    fix some issues of async behavior to provide result

--- bin/depshrink.js
+++ bin/depshrink.js
@@ -5,23 +5,23 @@ var program = require('commander'),
 
 var optionsMap = {
     list: { delete: false },
-    remove: { delete: false }
+    remove: { delete: true }
 }
 
 program
-    .arguments("<mode> <executable> [dirs...]")
-    .action(function(mode, executable, dirs) {
+    .arguments("<mode> <executable> <boottime> [dirs...]")
+    .action((mode, executable, boottime, dirs) => {
         console.log('execute: in mode('+ mode + ') the executable '+ executable + ' in ' + dirs)
         if(!optionsMap[mode])
             error(mode + ': command not supported')
         
-        depsShrink(executable,dirs,optionsMap[mode].delete,true, 1000).then((result) => {
+        depsShrink(executable, dirs, optionsMap[mode].delete, true, boottime).then((result) => {
             console.log('Not critical files: ')
             for(optional of result.optionals)
-            console.log('    - '+ optionals)
+                console.log('    - '+ optional)
             console.log('Critical files: ')
             for(notOptional of result.notOptionals)
-            console.log('    - '+ notOptional)
+                console.log('    - '+ notOptional)
         })
     })
     .parse(process.argv);
