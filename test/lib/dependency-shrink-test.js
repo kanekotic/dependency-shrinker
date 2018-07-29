80ae608b09a19d6e2d6bb58c9fe9b28be299e9dc
kanekotic <alvarojosepl@gmail.com>
2018-07-29 18:00:28 +0200

    organize into the different lists

--- test/lib/dependency-shrink-test.js
+++ test/lib/dependency-shrink-test.js
@@ -1,8 +1,10 @@
 jest.mock('../../lib/helpers/files', () => ({
     getAll: jest.fn()
 }))
+jest.mock('../../lib/helpers/execute', () => jest.fn())
 const shrinker = require('../../lib/dependency-shrink'),
     files = require('../../lib/helpers/files'),
+    execute = require('../../lib/helpers/execute'),
     faker = require('faker')
 
 describe('list files should', () => {
@@ -13,15 +15,24 @@ describe('list files should', () => {
     })
 
     it('return a list of files that are optional and non optional',async () => {
-        let fileList = [faker.random.uuid(), faker.random.uuid()]
+        let fileList = [faker.random.uuid(), faker.random.uuid()],
+            bootTime = faker.random.number(1000)
+            executable = faker.random.uuid(),
+            recursive = faker.random.boolean()
         files.getAll.mockReturnValue(fileList)
+        execute.mockReturnValueOnce(Promise.resolve({isOptional: false}))
+        execute.mockReturnValueOnce(Promise.resolve({isOptional: true}))
 
-        let result = shrinker(directories)
+        let result = await shrinker(executable, directories, recursive, bootTime)
         
-        expect(files.getAll).toBeCalledWith(directories)
-        expect(result.optionals).toBeDefined()
-        expect(result.notOptionals).toBeDefined()
-        
+        expect(files.getAll).toBeCalledWith(directories, recursive)
+        fileList.forEach(file => {
+            expect(execute).toBeCalledWith(executable, file, bootTime)
+        })
+        expect(result).toEqual({
+            optionals:[fileList[1]],
+            notOptionals:[fileList[0]]
+        })
     })
 
 })
--- test/lib/helpers/execute-test.js
+++ test/lib/helpers/execute-test.js
@@ -107,24 +107,3 @@ describe('execute should', () => {
 
     })
 })
-
-// function testExecute(executable, file, boottime){
-//     return new Promise( (resolve, reject) => {
-//         let timeoutId;
-//         let localFile = file;
-//         fs.renameSync(localFile, localFile+'.back', (err) => { if (err) reject(err) })
-//         let child  = exec(executable)
-//         child.on('exit', (code, signal) => {
-//             clearTimeout(timeoutId);
-//             fs.renameSync(localFile+'.back', localFile, (err) => { if (err) reject(err) })
-//             console.log(code)
-//             if (signal === 'SIGKILL' || code === 1) 
-//                 resolve({isOptional: true})
-//             else
-//                 resolve({isOptional: false})
-//         });
-//         timeoutId = setTimeout(() => { 
-//                 kill(child.pid, 'SIGKILL')
-//                 }, boottime)
-//     })
-// }
