001899d6bc08babcefb65977c8e6863d8d9e52ae
kanekotic <alvarojosepl@gmail.com>
2018-07-29 19:56:31 +0200

    simplified code

--- bin/depshrink.js
+++ bin/depshrink.js
@@ -5,24 +5,9 @@ var program = require('commander'),
     shrink = require('../lib/dependency-shrink')
 
 program
-    .version(packageJson.version)
-  
-//   program
-//     .command('<executable> <BootTime> [dirs...]')
-//     .description('converts raw guid to string format')
-//     .action((guids) => 
-//     guids.forEach(guid => {
-//       try {
-//         console.log(index.convertRaw(guid))
-//       } catch (_) {
-//         console.log(`${guid}: is not a valid raw format for a guid`)
-//       }
-//     })
-//   )
-
-program
+    .version(packageJson.version)  
     .arguments("<executable> <BootTime> [dirs...]")
-    .description('tests if a file is necesary to run a binary')
+    .description('tests if a file is necessary to run a binary')
     .action((executable, bootTime, dirs) => {
         console.log('executing '+ executable + ' in ' + dirs)
         shrink(executable, dirs, true, bootTime)
